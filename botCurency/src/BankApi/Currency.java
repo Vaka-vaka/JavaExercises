d8ca31438dad52760f44e3fca0a2f616f3879cbc
Vaka-vaka <Waka2006@ukr.net>
2021-09-20 17:14:55 +0300

    Create Currency.java

---
+++ botCurency/src/BankApi/Currency.java
@@ -0,0 +1,26 @@
+/**
+ * botCurrecy. Java.
+ *
+ * @autor Valentin Mozul
+ * @version from 20.09.21
+ */
+
+package BankApi;
+
+public enum Currency {
+
+    USD("USD"),
+    UAH("UAH"),
+    EUR("EUR"),
+    RUB("RUB");
+
+    String value;
+
+    Currency(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
