da17c5d0f8610f05289c289822d7fb458632d389
Vaka-vaka <Waka2006@ukr.net>
2021-09-21 17:26:09 +0300

    Update PrivatBankApi.java

--- botCurency/src/BankApi/PrivatBankApi.java
+++ botCurency/src/BankApi/PrivatBankApi.java
@@ -7,7 +7,14 @@
 
 package BankApi;
 
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
 import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -16,5 +23,27 @@ public class PrivatBankApi {
     private static final String url =  "https://api.privatbank.ua/p24api/exchange_rates?json&date="; //Архив курсов валют ПриватБанка
     private static final URI uri = URI.create(url + new SimpleDateFormat("dd.MM.yyyy").format(new Date()));
 
+    public static List<PrivatCurrency> getListOfCurrenciesRate() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        Gson gson = new Gson();
 
+        final HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        List<PrivatCurrency> currencies = gson.fromJson(response.body(), new TypeToken<List<PrivatCurrency>>() {
+        }.getType());
+
+        List<PrivatCurrency> result = new ArrayList<>();
+        for (PrivatBankApi currency : currencies) {
+            for (Currency value : Currency.values()) {
+                if (currency.getCc().equals(value.getValue())) {
+                    result.add(currency);
+                }
+            }
+        }
+        return result;
+    }
 }
